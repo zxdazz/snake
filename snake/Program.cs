693b5961d93b621ff8aeef93bf8309bed0a20572
avalor@mail.ru <avalor@mail.ru>
2018-04-04 12:22:35 +0300

    commit 3

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,7 +10,7 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi all!");
+            Console.WriteLine("Hi all, people!");
             Console.ReadKey();
 
         }
