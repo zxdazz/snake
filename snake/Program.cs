2cea76695944b286ef2fc30c7d70de00e39503fe
avalor@mail.ru <avalor@mail.ru>
2018-04-04 15:51:12 +0300

    tests with lists

--- snake/Program.cs
+++ snake/Program.cs
@@ -11,18 +11,38 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(3,5,'#');
-            /*p1.x = 3;
-            p1.y = 5;
-            p1.sym = '*'; */
             p1.Draw();
 
-
             Point p2 = new Point(5, 7,'@');
-            /*p2.x = 6;
-            p2.y = 8;
-            p2.sym = '#';*/
+           // p2.Draw();
 
-            p2.Draw();
+            //lists
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+
+            int l = numList[0];
+            int d = numList[1];
+
+            List<string> strList = new List<string>();
+            strList.Add("first");
+            strList.Add("second");
+
+            string fir = strList[1];
+            string sec = strList[1];
+
+            //Console.WriteLine(fir);
+            //Console.WriteLine(sec);
+
+            List<Point> points = new List<Point>();
+            points.Add(p1);
+            points.Add(p2);
+
+            Point p3 = new Point();
+            p3 = points[1];
+            p3.Draw();
+
 
 
             Console.ReadKey();
