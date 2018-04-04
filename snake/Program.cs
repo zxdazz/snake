449b7e390df5bbad0bcd55bd89bf6cd6fa6276cf
avalor@mail.ru <avalor@mail.ru>
2018-04-04 13:39:25 +0300

    function Draw

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,9 +10,26 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi all, people!");
+            int x1 = 3;
+            int y1 = 5;
+            char sym1 = '@';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 6;
+            int y2 = 3;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+
             Console.ReadKey();
 
+        }
+        static void Draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
